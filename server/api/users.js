5c4fe211de51a5284867208809789329a247f4be
Mike Wong <wongmdesign@gmail.com>
2019-01-08 15:21:56 -0500

    routes for all users/products & individual user/product

--- server/api/users.js
+++ server/api/users.js
@@ -1,16 +1,24 @@
 const router = require('express').Router()
-const {User} = require('../db/models')
+const { User } = require('../db/models')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
   try {
-    const users = await User.findAll({
-      // explicitly select only the id and email fields - even though
-      // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
-      attributes: ['id', 'email']
-    })
-    res.json(users)
+    if (req.user && req.user.isAdmin) {
+      const users = await User.findAll()
+      res.json(users)
+    }
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    if (req.user && req.user.isAdmin) {
+      const user = await User.findById(req.params.id)
+      res.json(user)
+    }
   } catch (err) {
     next(err)
   }
