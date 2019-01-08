f831f779cf16fc1adaa9bdfe381b28e5bc0c171f
Moshe Karten <moek613@gmail.com>
2019-01-08 16:15:24 -0500

    added route to get individual user's profile data

--- server/api/users.js
+++ server/api/users.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const { User } = require('../db/models')
+const {User} = require('../db/models')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
@@ -18,19 +18,7 @@ router.get('/:id', async (req, res, next) => {
     if (req.user && req.user.isAdmin) {
       const user = await User.findById(req.params.id)
       res.json(user)
-    }
-    if (req.user && req.admin) {
-      const users = await User.findAll()
-      res.json(users)
-    }
-  } catch (err) {
-    next(err)
-  }
-})
-
-router.get('/:id', async (req, res, next) => {
-  try {
-    if (req.user && req.admin) {
+    } else if (req.user.id === req.params.id) {
       const user = await User.findById(req.params.id)
       res.json(user)
     }
