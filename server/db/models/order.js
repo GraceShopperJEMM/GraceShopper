4a81a4a9b1741763a9af8db009bb271e1b4d4a08
Justin Taylor Goldberg <JustinTaylorG@Gmail.com>
2019-01-08 12:43:05 -0500

    Created Order model and set Many-to-One association with User

---
+++ server/db/models/order.js
@@ -0,0 +1,18 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Order = db.define('orders', {
+  address: {
+    type: Sequelize.STRING
+  },
+  email: {
+    type: Sequelize.STRING,
+    validate: {
+      isEmail: true
+    }
+  },
+  orderDate: {
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.NOW
+  }
+})
