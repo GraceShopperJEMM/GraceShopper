2fbcdd7d0ce10cd72f030aa4554043baf38ffe64
Justin Taylor Goldberg <JustinTaylorG@Gmail.com>
2019-01-10 23:20:11 -0500

    Converted Tabs to React-Router Links using component prop, eliminates unnecessary Redirects

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
-import {Redirect} from 'react-router-dom'
+import {Redirect, Link} from 'react-router-dom'
 import {logout, changeTab} from '../store'
 import {getProductView} from '../store/viewProduct'
 import {Tabs, Tab, AppBar} from '@material-ui/core'
@@ -16,16 +16,17 @@ class Navbar extends React.Component {
       <div>
         <AppBar position="static" style={{backgroundColor: '#0091d1'}}>
           <Tabs value={this.props.tab} onChange={this.props.handleChange}>
-            <Tab label="Products" onClick={this.props.seeAllProducts} />
-            <Tab label="Profile" />
-            <Tab label="Cart" />
-            <Tab label="Login" />
+            <Tab
+              label="Products"
+              onClick={this.props.seeAllProducts}
+              component={Link}
+              to="/products"
+            />
+            <Tab label="Profile" component={Link} to="/home" />
+            <Tab label="Cart" component={Link} to="/cart" />
+            <Tab label="Login" component={Link} to="/login" />
           </Tabs>
         </AppBar>
-        {this.props.tab === 0 && <Redirect to="/products" />}
-        {this.props.tab === 1 && <Redirect to="/home" />}
-        {this.props.tab === 2 && <Redirect to="/cart" />}
-        {this.props.tab === 3 && <Redirect to="/login" />}
       </div>
     )
   }
