72f4b0b50e9ae2e1e7be7ef8b27820b495a3d7dc
Mike Wong <wongmdesign@gmail.com>
2019-01-13 14:04:05 -0500

    cleaned up unused states/methods

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,52 +1,116 @@
-import React from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
+// import {modifyUser} from '../store/user'
 import {
   Card,
   CardContent,
   CardMedia,
   Typography,
-  IconButton
+  IconButton,
+  Button
 } from '@material-ui/core'
-import Button from '@material-ui/core/Button'
 
 // Components
 import AllProducts from './AllProducts'
+import UserForm from './UserForm'
 
 /**
  * COMPONENT
  */
-export const UserHome = props => {
-  const {email, name} = props
+class UserHome extends Component {
+  constructor() {
+    super()
+    this.state = {
+      // name: '',
+      // email: '',
+      viewForm: false
+    }
+    this.showEditForm = this.showEditForm.bind(this)
+    // this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
 
-  return (
-    <div id="profile">
-      <Typography color="primary" variant="h2">
-        Welcome {name} to Duck Sales!
-      </Typography>
-      <div className="details">
-        <Typography variant="h4">Profile Details</Typography>
+  // static getDerivedStateFromProps (props, state) {
+  //   return {
+  //     name: props.name,
+  //     email: props.email
+  //   }
+  // }
+
+  // handleChange (evt) {
+  //   // this state change targets any input field
+  //   this.setState({
+  //     [evt.target.name]: evt.target.value
+  //   })
+  // }
+
+  // toggle edit form state
+  showEditForm(evt) {
+    evt.preventDefault()
+    this.setState(prevState => ({
+      viewForm: !prevState.viewForm
+    }))
+  }
+
+  handleSubmit(evt) {
+    evt.preventDefault()
+    // this.props.modifyUser(this.state, this.props.match.params.id)
+  }
+
+  render(props) {
+    const {email, name} = this.props
+
+    return (
+      <div id="profile">
+        <Typography color="primary" variant="h2">
+          Welcome {name} to Duck Sales!
+        </Typography>
+        <div className="details">
+          <Typography variant="h4">Your Profile</Typography>
+        </div>
+        <div className="details">
+          <Typography variant="h6">Name:</Typography>
+          <Typography variant="p">{name}</Typography>
+        </div>
+        <div className="details">
+          <Typography variant="h6">User Email:</Typography>
+          <Typography variant="p">{email}</Typography>
+          {!this.state.viewForm ? (
+            <Button
+              variant="outlined"
+              color="secondary"
+              onClick={this.showEditForm}
+            >
+              Edit Profile
+            </Button>
+          ) : (
+            <Button
+              variant="contained"
+              color="default"
+              onClick={this.showEditForm}
+            >
+              Cancel Edit
+            </Button>
+          )}
+          {this.state.viewForm ? (
+            <UserForm
+              handleSubmit={this.handleSubmit}
+              handleChange={this.handleChange}
+              name={this.state.name}
+              email={this.state.email}
+            />
+          ) : (
+            ''
+          )}
+        </div>
+        <div className="details">
+          <Typography variant="h6">Order History:</Typography>
+          {/* Insert order history prop */}
+        </div>
       </div>
-      <div className="details">
-        <Typography variant="h6">Name:</Typography>
-        <Typography variant="p">{name}</Typography>
-        <Button variant="outlined" color="secondary">
-          Edit Name
-        </Button>
-      </div>
-      <div className="details">
-        <Typography variant="h6">User Email:</Typography>
-        <Typography variant="p">{email}</Typography>
-        <Button variant="outlined" color="secondary">
-          Edit Email
-        </Button>
-      </div>
-      <div className="details">
-        <Typography variant="h6">Order History:</Typography>
-        {/* Insert order history prop */}
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 /**
@@ -58,6 +122,12 @@ const mapState = state => {
     name: state.user.name
   }
 }
+
+// const mapDispatch = dispatch => {
+//   return {
+//     modifyUser: (user, id) => dispatch(modifyUser(user, id))
+//   }
+// }
 
 export default connect(mapState)(UserHome)
 
